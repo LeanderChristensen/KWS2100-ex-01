bbc5744e774ab356cb2e80b0619c46ac1ba71bb5
Leander Christensen <christensenleander@gmail.com>
2026-01-05 15:11:22 +0100

    Added base OpenLayers map

---
+++ src/application.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useRef, useState } from "react";
+import { Feature, Map, MapBrowserEvent, View } from "ol";
+import TileLayer from "ol/layer/Tile.js";
+import { OSM } from "ol/source.js";
+import VectorLayer from "ol/layer/Vector.js";
+import VectorSource from "ol/source/Vector.js";
+import { GeoJSON } from "ol/format.js";
+import { useGeographic } from "ol/proj.js";
+import "ol/ol.css";
+import { Circle, Fill, Stroke, Style, Text } from "ol/style.js";
+
+useGeographic();
+
+const map = new Map({
+  view: new View({ center: [10.8, 59.9], zoom: 13 }),
+  layers: [new TileLayer({ source: new OSM() })],
+});
+
+export function Application() {
+  const mapRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    map.setTarget(mapRef.current!);
+  }, []);
+
+  return <div ref={mapRef}></div>;
+}
